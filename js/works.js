9cf37358c1f61394ade7784c1d9b4bc16ced37b6
CHASE <chaunghareach@gmail.com>
2026-09-22 09:23:27 +0900

    Add in-site studio to upload images and prompts

--- js/works.js
+++ js/works.js
@@ -1,12 +1,8 @@
 const CONFIG = {
   github: "CHAsere96/BLANC",
   folder: "images",
-  useRemoteFolder: false
+  useRemoteFolder: true,
+  adminPin: "blanc"
 };
 
-const WORKS = [
-  { id: "01-wall", title: "wall", ratio: "9:16", src: "images/01-wall.jpg", prompt: "" },
-  { id: "02-tomatoes", title: "tomatoes", ratio: "9:16", src: "images/02-tomatoes.jpg", prompt: "" },
-  { id: "03-socks", title: "socks", ratio: "9:16", src: "images/03-socks.jpg", prompt: "" },
-  { id: "04-olive", title: "olive", ratio: "9:16", src: "images/04-olive.jpg", prompt: "" }
-];
+const WORKS = [];
